b6cfc3e051085d656c2623a9a371ef9c697e3706
Wojewódka Mateusz (STUD) <01103762@pw.edu.pl>
2019-09-08 01:10:47 +0200

    params fixed

--- pso/main.cpp
+++ pso/main.cpp
@@ -172,13 +172,17 @@ int main()
 //    boothConfig.iterationCount = 1000;
 //    TestPso(*logger, boothFunction, 2, boothConfig, "Expected min -> f(1,3) = 0");
 
-//    cout << "EASON FUNCTION" << endl;
-//    SwarmConfig easonConfig(*logger);
-//    easonConfig.particleFactors = new ParticleFactors(0.65,3,2);
-//    easonConfig.particlesCount = 200;
-//    easonConfig.iterationCount = 10000;
-//    easonConfig.velocityLimit = new VelocityLimit(10, VelocityLimitMode::LenghtLimit);
-//    TestPso(*logger, easonFunction, 2, easonConfig, "Expected min -> f(PI,PI) = -1");
+    cout << "EASON FUNCTION" << endl;
+    SwarmConfig easonConfig(*logger);
+    easonConfig.particleFactors = new ParticleFactors(0,3,2);
+    easonConfig.particlesCount = 50;
+    easonConfig.iterationCount = 1000;
+    std::vector<Domain> easonDomains;
+    easonDomains.push_back(Domain(-100, 100));
+    easonDomains.push_back(Domain(-100, 100));
+    easonConfig.variablesStartDomains = &easonDomains;
+    easonConfig.velocityLimit = new VelocityLimit(4, VelocityLimitMode::LenghtLimit);
+    TestPso(*logger, easonFunction, 2, easonConfig, "Expected min -> f(PI,PI) = -1");
 
 //    cout << "LEVI FUNCTION" << endl;
 //    SwarmConfig leviConfig(*logger);
@@ -192,14 +196,11 @@ int main()
     mcCornickConfig.particleFactors = new ParticleFactors(0,3,3);
     mcCornickConfig.particlesCount = 50;
     mcCornickConfig.iterationCount = 1000;
-
-    std::vector<Domain> domains;
-    domains.push_back(Domain(-1.5, 4));
-    domains.push_back(Domain(-3, 4));
-    mcCornickConfig.variablesStartDomains = &domains;
-
+    std::vector<Domain> mcCornickDomains;
+    mcCornickDomains.push_back(Domain(-1.5, 4));
+    mcCornickDomains.push_back(Domain(-3, 4));
+    mcCornickConfig.variablesStartDomains = &mcCornickDomains;
     mcCornickConfig.velocityLimit = new VelocityLimit(2, VelocityLimitMode::StandardLimit);
-
     TestPso(*logger, mcCornickFunction, 2, mcCornickConfig, "Expected min -> f(-0.54719,-1.54719) = -1.9133");
 
     delete logger;
